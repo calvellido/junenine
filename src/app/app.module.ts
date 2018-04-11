45362d63e08302726b3b76a1a18098907025e17b
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2018-04-11 23:15:48 +0200

    Include basic Angular Material additions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatRadioModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
